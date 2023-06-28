793ef4fc4a42af5d38ef5393b123ab11ca7c990e
Bodhi14 <bodhi140802@gmail.com>
2023-06-29 02:38:33 +0530

    Removed local port

--- server/index.js
+++ server/index.js
@@ -41,8 +41,6 @@ app.delete("/api/:id", async(req, res)=> {
    res.send(result);
 })
 
-const port = process.env.PORT;
-
 app.listen(port, () => {
    console.log("Server is running on port " + port);
 });
